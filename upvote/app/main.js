7a1a5fdbb3470617ea3948aa6f7b8c63a75f3704
codepleb <codepleb4@gmail.com>
2018-03-18 09:47:09 +0100

    p38

--- upvote/app/main.js
+++ upvote/app/main.js
@@ -3,13 +3,13 @@ const submissionComponent = {
   ` <div style="display: flex; width: 100%">
       <figure class="media-left">
         <img class="image is-64x64"
-          v-bind:src="submission.submissionImage">
+          :src="submission.submissionImage">
       </figure>
       <div class="media-content">
         <div class="content">
           <p>
             <strong>
-              <a v-bind:href="submission.url" class="has-text-info">
+              <a :href="submission.url" class="has-text-info">
                 {{ submission.title }}
               </a>
               <span class="tag is-small">#{{ submission.id }}</span>
@@ -20,13 +20,13 @@ const submissionComponent = {
             <small class="is-size-7">
               Submitted by:
               <img class="image is-24x24"
-                v-bind:src="submission.avatar">
+                :src="submission.avatar">
             </small>
           </p>
         </div>
       </div>
       <div class="media-right">
-        <span class="icon is-small" v-on:click="upvote(submission.id)">
+        <span class="icon is-small" @click="upvote(submission.id)">
           <i class="fa fa-chevron-up"></i>
           <strong class="has-text-info">{{ submission.votes }}</strong>
         </span>
