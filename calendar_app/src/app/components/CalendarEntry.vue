13828876127a01b6f4e3f60dde4575f1cc527597
codepleb <codepleb4@gmail.com>
2018-03-24 17:16:42 +0100

    p95

--- calendar_app/src/app/components/CalendarEntry.vue
+++ calendar_app/src/app/components/CalendarEntry.vue
@@ -1,11 +1,18 @@
 <template>
-<div id="calendar-entry">
-  <div class="calendar-entry-note">
-    <input type="text" placeholder="New Event" v-model="inputEntry" required />
-    <p class="calendar-entry-day">Day of event: <span class="bold">{{titleOfActiveDay}}</span></p>
-    <a class="button is-primary is-small is-outlined" @click="submitEvent(inputEntry)">Submit</a>
-  </div>
-</div>
+    <div id="calendar-entry">
+      <div class="calendar-entry-note">
+        <input type="text" placeholder="New Event" v-model="inputEntry" required />
+        <p class="calendar-entry-day">
+            Day of event: <span class="bold">{{titleOfActiveDay}}</span>
+        </p>
+        <a class="button is-primary is-small is-outlined" @click="submitEvent(inputEntry)">
+            Submit
+        </a>
+      </div>
+        <p style="color: red; font-size: 13px" v-if="error">
+            You must type something first!
+        </p>
+    </div>
 </template>
 
 
@@ -15,19 +22,22 @@ import {store} from '../store';
 export default {
     name: 'CalendarEntry',
     computed: {
-        titleOfActiveDay () {
+        titleOfActiveDay() {
             return store.getActiveDay().fullTitle;
         }
     },
-    data () {
+    data() {
         return {
-            inputEntry: ''
+            inputEntry: '',
+            error: false
         }
     },
     methods: {
         submitEvent(eventDetails) {
+            if (eventDetails === '') return this.error = true;
             store.submitEvent(eventDetails);
             this.inputEntry = '';
+            this.error = false;
         }
     }
 }
--- calendar_app/src/app/components/CalendarEvent.vue
+++ calendar_app/src/app/components/CalendarEvent.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="day-event" :style="getEventBackgroundColor">
-        <div>
+        <div v-if="!event.edit">
             <span class="has-text-centered details">{{ event.details }}</span>
             <div class="has-text-centered icons">
-                <i class="fa fa-pencil-square edit-icon"></i>
-                <i class="fa fa-trash-o delete-icon"></i></div>
+                <i class="fa fa-pencil-square edit-icon" @click="editEvent(day.id, event.details)"></i>
+                <i class="fa fa-trash-o delete-icon"></i>
+            </div>
+        </div>
+        <div v-if="event.edit">
+            <input type="text" :placeholder="event.details" />
+            <div class="has-text-centered icons">
+                <i class="fa fa-check"></i>
+            </div>
         </div>
     </div>
 </template>
+
+
 <script>
+    import {store} from '../store';
+
     export default {
         name: 'CalendarEvent',
         props: ['event', 'day'],
@@ -18,9 +29,16 @@
                 let randomColor = colors[Math.floor(Math.random() * colors.length)];
                 return `background-color: ${randomColor}`;
             }
+        },
+        methods: {
+            editEvent (dayId, eventDetails) {
+                store.editEvent(dayId, eventDetails);
+            }
         }
     }
 </script>
+
+
 <style lang="scss" scoped> .day-event {
     margin-top: 6px;
     margin-bottom: 6px;
