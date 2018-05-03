dbb7aa6f0f91ec3542f7a91f58c243e66a879c39
codepleb <codepleb4@gmail.com>
2018-05-04 00:09:14 +0200

    p236

--- form_handling/app/main.js
+++ form_handling/app/main.js
@@ -12,7 +12,7 @@ const InputForm = {
           <label>Email</label>
           <input v-model="fields.email" type="text"
                  placeholder="What's your email?"/>
-           <span style="color: red">{{ fieldErrors.newItem }}</span>
+           <span style="color: red">{{ fieldErrors.email }}</span>
         </div>
         <div class="field">
           <label>Urgency</label>
@@ -22,13 +22,13 @@ const InputForm = {
             <option>Moderate</option>
             <option>Urgent</option>
           </select>
-          <span style="color: red">{{ fieldErrors.newItem }}</span>
+          <span style="color: red">{{ fieldErrors.urgency }}</span>
         </div>
         <div class="field">
           <div class="ui checkbox">
             <input v-model="fields.termsAndConditions" type="checkbox"/>
             <label>I accept the terms and conditions</label></div>
-            <span style="color: red">{{ fieldErrors.newItem }}</span>
+            <span style="color: red">{{ fieldErrors.termsAndConditions }}</span>
         </div>
         <button class="ui button">Submit</button>
       </form>
@@ -53,13 +53,33 @@ const InputForm = {
         termsAndConditions: undefined
       },
       items: []
-    }
+    };
   },
   methods: {
     submitForm(evt) {
-      this.items.push(this.newItem);
-      this.newItem = '';
       evt.preventDefault();
+      this.fieldErrors = this.validateForm(this.fields);
+      if (Object.keys(this.fieldErrors).length) return;
+      this.items.push(this.fields.newItem);
+      this.fields.newItem = '';
+      this.fields.email = '';
+      this.fields.urgency = '';
+      this.fields.termsAndConditions = false;
+    },
+    validateForm(fields) {
+      const errors = {};
+      if (!fields.newItem) errors.newItem = "New Item Required"; if (!fields.email) errors.email = "Email Required";
+      if (!fields.urgency) errors.urgency = "Urgency Required"; if (!fields.termsAndConditions) {
+        errors.termsAndConditions = "Terms and conditions have to be approved";
+      }
+      if (fields.email && !this.isEmail(fields.email)) {
+        errors.email = "Invalid Email";
+      }
+      return errors;
+    },
+    isEmail(email) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
     }
   }
 };
