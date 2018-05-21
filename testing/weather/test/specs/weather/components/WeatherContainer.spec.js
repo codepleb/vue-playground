e668a9c1de35bb97065fa9295a8129c944193242
codepleb <codepleb4@gmail.com>
2018-05-21 22:49:05 +0200

    p419

--- testing/weather/test/specs/weather/components/WeatherContainer.spec.js
+++ testing/weather/test/specs/weather/components/WeatherContainer.spec.js
@@ -1,5 +1,39 @@
 import WeatherContainer from '@/components/WeatherContainer';
+import {shallow, createLocalVue} from 'vue-test-utils';
+import Vuex from 'vuex';
 
 describe('WeatherContainer.vue', () => {
-  
+  let wrapper;
+  let getters;
+  let actions;
+  let store;
+  let localVue;
+
+  const setUpWrapper = loading => {
+    localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    getters = {
+      location: () => 'New York',
+      weatherDescription: () => 'Light Cloud',
+      imageAbbr: () => 'lc.png',
+      weatherTemp: () => -10.0,
+      loading: () => loading
+    };
+
+    actions = {
+      fetchWeather: sinon.stub()
+    };
+
+    store = new Vuex.Store({
+      getters,
+      actions
+    });
+
+    wrapper = shallow(WeatherContainer, {
+      localVue,
+      store
+    });
+  };
+
 });
