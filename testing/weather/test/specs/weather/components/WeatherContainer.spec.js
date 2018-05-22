09b5b02a4487cd66f302b906ab644221f0022dee
codepleb <codepleb4@gmail.com>
2018-05-22 21:58:24 +0200

    p425

--- testing/weather/test/specs/weather/components/WeatherContainer.spec.js
+++ testing/weather/test/specs/weather/components/WeatherContainer.spec.js
@@ -36,4 +36,48 @@ describe('WeatherContainer.vue', () => {
     });
   };
 
+  it('should render the correct content when the app is loading', () => {
+    setUpWrapper(true);
+    expect(
+      wrapper.html()
+    ).to.contain('<div class="loader"></div>');
+    expect(
+      wrapper.html()
+    ).to.not.contain('<h1 class="subtitle weather__city">New York</h1>');
+    expect(
+      wrapper.html()
+    ).to.not.contain('<p class="weather__description">Light Cloud</p>');
+    expect(
+      wrapper.html()
+    ).to.not.contain('<p class="weather__temperature">-10 ºC</p></div>');
+  });
+
+  it('should render the correct content when the app is not loading', () => {
+    setUpWrapper(false);
+    expect(
+      wrapper.html()
+    ).to.contain('<h1 class="subtitle weather__city">New York</h1>');
+    expect(
+      wrapper.html()
+    ).to.contain('<p class="weather__description">Light Cloud</p>');
+    expect(
+      wrapper.html()
+    ).to.contain('<p class="weather__temperature">-10 ºC</p></div>');
+    expect(
+      wrapper.html()
+    ).to.not.contain('<div class="loader"></div>');
+  });
+
+  it('should call the "fetchWeather" action once when created', () => {
+    setUpWrapper(false);
+    expect(actions.fetchWeather).to.have.been.calledOnce;
+  });
+
+  it('should also call the "fetchWeather" action when "id" is changed', () => {
+    setUpWrapper(false);
+
+    wrapper.setData({ id: '1398823'});
+    expect(actions.fetchWeather).to.have.been.calledTwice;
+  });
+
 });
