5d52ab56720116481f44ea1d12fc4841fb5880c7
codepleb <codepleb4@gmail.com>
2018-05-27 14:32:07 +0200

    5m40s

--- simple-coin-cap/simple-coin-cap/src/store.js
+++ simple-coin-cap/simple-coin-cap/src/store.js
@@ -5,12 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    coins: [],
+    totalMarketCap: 0,
+    total24HrVolume: 0,
+    totalBTCPercentage: 0,
+    loading: false,
   },
+  // Mutating store state
   mutations: {
+    UPDATE_COINS(state, payload) {
+
+    }
+  },
+  // Components dispatch actions that commits to mutations
+  actions: {
 
   },
-  actions: {
+  // Help compute store state, tha components can directly retrieve
+  getter: {
 
   }
 })
