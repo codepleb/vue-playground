a2fab600197644010abb035a9b2e8ce943e261d5
codepleb <codepleb4@gmail.com>
2018-05-30 23:23:33 +0200

    31m16s

--- simple-coin-cap/simple-coin-cap/src/views/home/home-page.vue
+++ simple-coin-cap/simple-coin-cap/src/views/home/home-page.vue
@@ -5,20 +5,38 @@
         <div class="ui three column grid">
           <div class="column">
             <div class="ui segment banner">
-              Market Cap: {{totalMarketCap}}
+              Market Cap: {{formatCurrency(totalMarketCap)}}
             </div>
           </div>
           <div class="column">
             <div class="ui segment banner">
-              Volume (24hr): {{total24HrVolume}}
+              Volume (24hr): {{formatCurrency(total24HrVolume)}}
             </div>
           </div>
           <div class="column">
             <div class="ui segment banner">
-              BTC Dominance: {{totalBTCPercentage}}
+              BTC Dominance: {{totalBTCPercentage}} %
             </div>
           </div>
         </div>
+
+        <div class="sixteen wide column">
+          <div class="ui segment header desktop">
+            <div class="ui grid">
+              <div class="one wide column">#</div>
+              <div class="two wide column">Name</div>
+              <div class="one wide column">Symbol</div>
+              <div class="three wide column">Market Cap</div>
+              <div class="two wide column">Price</div>
+              <div class="three wide column">Circulating Supply</div>
+              <div class="two wide column">% 1h</div>
+              <div class="two wide column">% 24h</div>
+            </div>
+          </div>
+        </div>
+
+        <home-coin-item v-for="coin in coins" :key="coin.id" :coin="coin" :coinQuotes="coin.quotes.USD" />
+
       </div>
     </div>
   </div>
@@ -26,15 +44,25 @@
 
 <script>
   import {mapGetters} from 'vuex';
+  import utils from '@/utils';
+  import HomeCoinItem from './components/home-coin-item';
 
   export default {
     name: 'HomePage',
+    created() {
+      this.formatCurrency = utils.formatCurrency;
+    },
     computed: {
       ...mapGetters([
+        'coins',
         'totalMarketCap',
         'total24HrVolume',
         'totalBTCPercentage',
       ])
+    },
+    components: {
+      HomeCoinItem,
+      'home-coin-item': HomeCoinItem
     }
   }
 </script>
